1d60f22f26c8fed7e1ea212b74ad311f4c12d5d6
Stefan Radovic <radovicstef@gmail.com>
2021-04-18 20:18:35 +0200

    Added method for calculating number of reads

--- IluminaPairEndSequencingSimulator/main.py
+++ IluminaPairEndSequencingSimulator/main.py
@@ -3,25 +3,38 @@
 
 # Methods
 def read_genome(file_name):
-    global genome
+    global genome, genome_length
     genome_file = open(file_name, "r")
-    lines = genome_file.read().splitlines() #returns the list of lines without \n
+    lines = genome_file.read().splitlines()  # returns the list of lines without \n
     for line in lines:
         # get only the nucleotides
         if line.find('>') == -1:
             genome = genome + line
     genome_file.close()
+    genome_length = len(genome)
+
+
+def calculate_number_of_reads(coverage, read_length):
+    global genome_length, num_of_reads, num_of_pair_end_reads
+    num_of_reads = int(round(coverage * genome_length / (read_length * 2)))  # Lander-Waterman formula
+    num_of_pair_end_reads = 2 * num_of_reads
 
 
 # Global variables
 genome = ""
+genome_length = 0
+num_of_reads = 0  # number of reads = number of fragments
+num_of_pair_end_reads = 0  # number of pair-end reads = 2 * number of reads
 
 
 # The main program
-def main():
+def simulate():
     global genome
     read_genome("genomeSample.fa")
-    print("Genome from FASTA file: {}".format(genome))
+    calculate_number_of_reads(4, 7)
+    print("Number of reads: {}".format(num_of_reads))
+    print("Genome size from FASTA file: {}".format(genome_length))
+
 
 if __name__ == "__main__":
-    main()
+    simulate()
