d77ae8c3111bec21deb7d124f24968feec06ba1b
Stefan Radovic <radovicstef@gmail.com>
2021-04-22 17:13:16 +0200

    Added logic to mutate genome based on snv, insert, delete probability

--- IluminaPairEndSequencingSimulator/main.py
+++ IluminaPairEndSequencingSimulator/main.py
@@ -3,6 +3,7 @@
 
 # Methods
 import numpy.random
+from random import randrange
 
 
 def read_genome(file_name):
@@ -56,8 +57,35 @@ def get_quality(avg_quality, sigma, read_length):
     return "".join([chr(quality+33) for quality in qualities])
 
 
+def get_mutated_single_nucleotide(nucleotide):
+    bases = ["A", "C", "G", "T", "N"]
+    bases.remove(nucleotide)
+    return bases[randrange(4)]
+
+
+def add_mutations(prob_snv=0, prob_ins=0, prob_del=0):
+    bases = ['A', 'C', 'G', 'T']
+    for sequence_name in genome:
+        sequence_length = len(genome[sequence_name])
+        snv_num = round(sequence_length * prob_snv)
+        ins_num = round(sequence_length * prob_ins)
+        del_num = round(sequence_length * prob_del)
+
+        for i in range(snv_num):
+            mutation_position = randrange(sequence_length-1)
+            genome[sequence_name] = genome[sequence_name][0:mutation_position] + get_mutated_single_nucleotide(genome[sequence_name][mutation_position]) + genome[sequence_name][mutation_position+1:]
+
+        for i in range(ins_num):
+            mutation_position = randrange(sequence_length - 1)
+            genome[sequence_name] = genome[sequence_name][0:mutation_position] + bases[randrange(4)] + genome[sequence_name][mutation_position:]
+
+        for i in range(del_num):
+            mutation_position = randrange(sequence_length - 1)
+            genome[sequence_name] = genome[sequence_name][0:mutation_position] + genome[sequence_name][mutation_position+1:]
+
+
 # Global variables
-genome = {}  # dictionary - sequence_name:sequence
+genome = {}  # dictionary - sequence_name:sequence from FASTA file
 genome_length = 0
 num_of_reads = 0  # number of reads = number of fragments
 num_of_pair_end_reads = 0  # number of pair-end reads = 2 * number of reads
--- IluminaPairEndSequencingSimulator/test.py
+++ IluminaPairEndSequencingSimulator/test.py
@@ -7,8 +7,11 @@ main.simulate("genomeSample.fa", 100, 4, 7, 10)
 
 # Test if sample genome is correctly extracted from FASTA file
 valid_genome_file = open('genome.txt', 'r')
-valid_genome = valid_genome_file.readline()
-assert(valid_genome == main.genome)
+valid_genome = [line.rstrip('\n') for line in valid_genome_file.readlines()]
+i = 0
+for sequence_name, sequence in main.genome.items():
+    assert sequence == valid_genome[i]
+    i += 1
 
 # Test reverse read
 read = "ACGTTTANAGAC"
@@ -27,3 +30,18 @@ qualities = main.get_quality(avg_quality, sigma, read_length)
 print(qualities)
 for quality in qualities:
     assert (quality >= chr(avg_quality - sigma) or quality <= chr(avg_quality + sigma))
+
+
+# Test getting the mutated single nucleotide
+assert main.get_mutated_single_nucleotide("A") != "A"
+assert main.get_mutated_single_nucleotide("C") != "C"
+assert main.get_mutated_single_nucleotide("G") != "G"
+assert main.get_mutated_single_nucleotide("T") != "T"
+assert main.get_mutated_single_nucleotide("N") != "N"
+
+
+# Test the mutations
+main.add_mutations(0.2)
+print("MUTATED GENOME: ")
+for sequence_name in main.genome:
+    print(main.genome[sequence_name])
